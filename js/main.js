36457a7584d809565397db8517b451d1ee851039
jiangtianyou <420671626@qq.com>
2016-12-20 17:10:02 +0800

    ...

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-document.write('<h1>Hello World</h1>');
+import '../css/test.scss';
+var x = [1,2,3].map(function(a){
+    return '<p>' + a + '</p>'
+})
+document.write(x[1]);
+
+class test{
+    say(){
+        console.log('say something!')
+    }
+}
+
+//测试结构赋值
+var a = [1,2,3]
+var b = [...a,4,5,6]
+console.log('b',b)
+
+//测试for...of
+var x = [1,2,3]
+for(var value of x){
+    console.log('value',value)
+}
+
+//测试let 和 const
+let lt = 'let value';
+const ct = 'const value';
+console.log(lt,ct);
--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,18 @@ module.exports = {
     entry: './js/main.js',
     output: {
         filename: 'bundle.js'
+    },
+    module: {
+        loaders:[
+            {
+                test: /\.js[x]?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader?presets[]=es2015&presets[]=react'
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
     }
 };
